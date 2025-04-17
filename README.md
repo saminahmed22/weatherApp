26f7b7e9e7904d0191d47d308df35fce0eb5e3f8
Samin Ahmed <saminahmed1001@gmail.com>
2025-04-17 19:26:59 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpackTemp
+
+Template for webpack
---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs}"] },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    rules: {
+      "no-unused-vars": "0",
+      "no-undef": "0",
+    },
+  },
+]);
